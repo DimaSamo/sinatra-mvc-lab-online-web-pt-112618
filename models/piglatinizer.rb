abc32c177e58ce2326692f8618c3ea42e64a303a
Dmitri Samotessov <dimasamo96@gmail.com>
2019-03-19 02:30:02 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -7,9 +7,10 @@ class PigLatinizer
     words_array=text.split(" ")
     words_array.map do |word|
       letters_array=word.split("")
+      original_array = letters_array
       letters_array.push(letters_array.delete_at(0)) unless /^[^aeiou\W]/i.match(letters_array.first) == nil
       letters_array.push(letters_array.delete_at(0)) if /^[^aeiou\W]/i.match(letters_array.first) != nil
-      letters_array.push("w") if /^[^aeiou\W]/i.match(letters_array.first) == nil
+      letters_array.push("w") if /^[^aeiou\W]/i.match(original_array.first) == nil
       letters_array.push("a")
       letters_array.push("y")
       letters_array.join("")
