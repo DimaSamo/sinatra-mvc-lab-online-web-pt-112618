032dfd6113ce90aaca9864b6132f4aa7d2a80e84
Dmitri Samotessov <dimasamo96@gmail.com>
2019-03-19 02:11:49 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -9,7 +9,9 @@ class PigLatinizer
     words_array.map do |word|
       letters_array=word.split("")
       letters_array.push(letters_array.delete_at(0))
+      letters_array.push("a")
+      letters_array.push("y")
       letters_array.join("")
-    end.join(" ")+"ay"
+    end.join(" ")
   end
 end
