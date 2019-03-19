e97f280a8c97a00209adbdeff1be7d3c181be82b
Dmitri Samotessov <dimasamo96@gmail.com>
2019-03-19 02:31:19 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -10,7 +10,7 @@ class PigLatinizer
       original_array = letters_array
       letters_array.push(letters_array.delete_at(0)) unless /^[^aeiou\W]/i.match(letters_array.first) == nil
       letters_array.push(letters_array.delete_at(0)) if /^[^aeiou\W]/i.match(letters_array.first) != nil
-      #letters_array.push("w") if /^[^aeiou\W]/i.match(original_array.first) == nil
+      letters_array.push("w") if /^[^aeiou\W]/i.match(original_array.first) == nil
       letters_array.push("a")
       letters_array.push("y")
       letters_array.join("")
