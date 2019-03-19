f9fd93ad62659c426e2592d9618c60d094b9ff70
Dmitri Samotessov <dimasamo96@gmail.com>
2019-03-19 02:35:20 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -9,7 +9,9 @@ class PigLatinizer
     words_array.map do |word|
       letters_array=word.split("")
       original_array = word.split("")
-      while /^[^aeiou\W]/i.match(letters_array.first) != nil {letters_array.push(letters_array.delete_at(0))}
+      while /^[^aeiou\W]/i.match(letters_array.first) != nil
+         letters_array.push(letters_array.delete_at(0))
+      end
       #letters_array.push(letters_array.delete_at(0)) if /^[^aeiou\W]/i.match(letters_array.first) != nil
       #binding.pry
       letters_array.push("w") if /^[^aeiou\W]/i.match(original_array.first) == nil
